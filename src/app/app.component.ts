1f0b6b7a232775bc79160d25bb8b81b6f1b66ac7
JerryLee <jerry.lee@ardomusnet.com>
2021-05-31 12:57:49 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import * as d3 from 'd3';
 
+interface test {
+  a: number;
+  b: number;
+  c: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'd3-demo';
+export class AppComponent implements OnInit {
+
+  svg: any;
+
+  xScale: any;
+  yScale: any;
+  padding: number = 10;
+
+  testA: test = {
+    a: 50,
+    b: 50,
+    c: 50
+  };
+
+  ngOnInit(): void {
+    this.initSvg();
+    this.initAxis();
+  }
+
+  initSvg() {
+    this.svg = d3.select('.demo').append('svg')
+      .attr('width', 600)
+      .attr('height', 600)
+  }
+
+  initAxis() {
+    this.xScale = d3.scaleLinear().domain([-50, 50]).range([0 + this.padding, 600 - this.padding]);
+    this.yScale = d3.scaleLinear().domain([50, -50]).range([0 + this.padding, 600 - this.padding]);
+    this.svg.append('g')
+      .attr('transform', 'translate('+ 0 +','+ this.yScale(0) +')')
+      .call(d3.axisBottom(this.xScale));
+    
+    this.svg.append('g')
+      .attr('transform', 'translate('+ this.xScale(0) +','+ 0 +')')
+      .call(d3.axisLeft(this.yScale));
+  }
+
+  valueChange() {
+    console.log(this.testA);
+  }
 }
